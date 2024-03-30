4f9d7ab828b01400e778f385e2160fbcdf9ae831
Sania Sadaqat <saniasadaqat3526@gmail.com>
2024-03-30 13:11:37 +0500

    Update Employeeattendance.js

--- src/models/Employeeattendance.js
+++ src/models/Employeeattendance.js
@@ -27,7 +27,6 @@ const EmployeeAttendanceSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-<<<<<<< HEAD
   salaryType: { 
     type: String,
     // required: true
@@ -58,82 +57,6 @@ const EmployeeAttendanceSchema = new mongoose.Schema({
   normalRate: {
     type: Number
   },
-=======
-  
-  // Additional fields for hourly employees
-  normalStartTime: {
-    type: String,
-
-    // required: true
-  },
-  normalEndTime: {
-    type: String,
-    // required: true
-  },
-  overtimeStartTime: {
-    type: String,
-    // required: true
-  },
-  overtimeEndTime: {
-    type: String,
-    // required: true
-  },
-  monthlySalary: {
-    type: Number
-  },
-  overRate: {
-    type: Number
-  },
-  normalRate: {
-    type: Number
-  },
-
-<<<<<<< Updated upstream
-    required: true,
-    default: '00:00'
-  },
-  normalEndTime: {
-    type: String,
-    required: true,
-    default: '00:00'
-  },
-  overtimeStartTime: {
-    type: String,
-    required: true,
-    default: '00:00'
-  },
-  overtimeEndTime: {
-    type: String,
-    required: true,
-    default: '00:00'
-  }
-
-    // required: true
-  },
-  normalEndTime: {
-    type: String,
-    // required: true
-  },
-  overtimeStartTime: {
-    type: String,
-    // required: true
-  },
-  overtimeEndTime: {
-    type: String,
-    // required: true
-  },
-  monthlySalary: {
-    type: Number
-  },
-  overRate: {
-    type: Number
-  },
-  normalRate: {
-    type: Number
-  },
->>>>>>> Stashed changes
->>>>>>> 5c3b08ee6f3d656a255ee1c31203ff6208a9271d
->>>>>>> 78e71b4e9e1252a71ba42dc22fed382409b67333
 });
 
 // Create and export the EmployeeAttendance model
